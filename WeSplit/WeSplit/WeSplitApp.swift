7eb7cb8b964898a4e1c4260b5abcdf7a8eb14fd9
Alireza <mr.toghyiani@icloud.com>
2020-07-14 18:47:15 +0430

    Start Day 16

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by ali on 7/14/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
